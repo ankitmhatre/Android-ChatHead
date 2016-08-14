5d3f1766ec17dac569bb98715aac7f0662138aad
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2016-08-14 17:50:39 +0530

    Useless TypeCasting

--- app/src/main/java/henrychuang/tw/chatheadmsg/MyDialog.java
+++ app/src/main/java/henrychuang/tw/chatheadmsg/MyDialog.java
@@ -31,7 +31,7 @@ public class MyDialog extends Activity {
 
 		edt = (EditText) findViewById(R.id.dialog_edt);
 		btn = (Button) findViewById(R.id.dialog_btn);
-		top = (View)findViewById(R.id.dialog_top);
+		top = findViewById(R.id.dialog_top);
 				
 		myDialog = MyDialog.this;
 		
