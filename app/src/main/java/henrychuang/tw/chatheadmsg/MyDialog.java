72e66affbae53d54fe5848417d7e72dacea3a754
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2016-08-14 17:48:35 +0530

    Added Function for Margin

--- app/src/main/java/henrychuang/tw/chatheadmsg/MyDialog.java
+++ app/src/main/java/henrychuang/tw/chatheadmsg/MyDialog.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 
 public class MyDialog extends Activity {
@@ -22,9 +24,11 @@ public class MyDialog extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		
 		setContentView(R.layout.dialog);
-		
+
+		LinearLayout linearLayout = (LinearLayout) findViewById(R.id.main_container);
+		setMargins(linearLayout, 0, 0, 0, 0);     //Set The Margin of the dialog according to your need.
+
 		edt = (EditText) findViewById(R.id.dialog_edt);
 		btn = (Button) findViewById(R.id.dialog_btn);
 		top = (View)findViewById(R.id.dialog_top);
@@ -57,7 +61,14 @@ public class MyDialog extends Activity {
 		});
 		
 	}
-		
+
+	private void setMargins(View view, int left, int top, int right, int bottom) {
+		if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+			ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+			p.setMargins(left, top, right, bottom);
+			view.requestLayout();
+		}
+	}
 	
 	@Override
 	protected void onResume() {
